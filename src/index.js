fc84029bd75dd100a91beb720d4811b5cc504383
Yegorantonyuk <yegorantonyuk@gmail.com>
2018-11-06 16:44:58 +0200

    Min max sum

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-console.log('start');
+export function min (numbers) {
+	if (numbers === undefined || numbers.length === 0) {
+		return undefined;
+	}
+	const correctNumbers = numbers.filter(element => {
+		return typeof element === 'number';
+	});
+	let minNumber = correctNumbers[0];
 
-function average () {
-
+	correctNumbers.forEach(element => {
+		if (element < minNumber) {
+			minNumber = element;
+		}
+	});
+	return minNumber;
 }
+export function max (numbers) {
+	if (numbers === undefined || numbers.length === 0) {
+		return undefined;
+	}
+	const correctNumbers = numbers.filter(element => {
+		return typeof element === 'number';
+	});
+	let maxNumber = correctNumbers[0];
 
-console.log('finish');
+	correctNumbers.forEach(element => {
+		if (element > maxNumber) {
+			maxNumber = element;
+		}
+	});
+	return maxNumber;
+}
+export function sum () {
+	const numbers = Array.from(arguments);
 
-setInterval(function () {}, 10);
+	if (numbers === undefined || numbers === 0) {
+		return undefined;
+	}
+	const correctNumbers = numbers.filter(element => {
+		return typeof element === 'number';
+	});
+	let sumnum = 0;
+
+	correctNumbers.forEach(element => {
+		sumnum += element;
+	});
+	return sumnum;
+}
